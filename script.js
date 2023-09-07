041b6ccefeef955b19f55070eeaedec8c2bebdce
James Jegede <jexzyjames@gmail.com>
2023-09-07 13:12:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ var utc = document.getElementById('time')
 var days = ['days', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 let show = new Date().getDay();
+let shoe = new Date();
 let curret = new Date();
 console.log(curret);
 // console.log(show);
 
+let month = days[shoe.getMonth()];
 
+utc.innerHTML = month;
 for (let index = 0; index < days.length; index++) {
 
     function result(){
